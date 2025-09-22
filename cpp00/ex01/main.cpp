7857fc734b2d75643b9618265560fafe55155e65
kinga <kinga.kwasniak5@gmail.com>
2025-09-22 12:11:02 +0200

    doing ex01 in cpp00 in progress

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -1,3 +1,34 @@
 #include <iostream>
 #include "PhoneBook.hpp"
 #include <iomanip>
+
+int main()
+{
+	std:: string cmd;
+	std::cout << "Welcome to Phone Book !";
+	std::cout << "Commands: ADD, SEARCH, EXIT"; // add this to loop
+	std::cout << std::endl;
+	while(true)
+	{
+		std:: cin >> cmd;
+		if (cmd == "ADD")
+		{
+
+		}
+		else if (cmd == "SEARCH")
+		{
+
+		}
+		else if (cmd == "EXIT")
+		{
+			std::cout << "Exiting the Phone Book";
+			return(0);
+		}
+		else
+		{
+			std::cout << "Unknown command. Please try again." << std::endl;
+			std::cout << "Commands: ADD, SEARCH, EXIT";
+		}
+	}
+	return(0);
+}

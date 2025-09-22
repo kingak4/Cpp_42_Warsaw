f1e7f6b788c33ee6dad9431ff67c53fb461f5a5f
kinga <kinga.kwasniak5@gmail.com>
2025-09-22 16:54:08 +0200

    adding contacts working

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -1,7 +1,39 @@
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+#include <cctype>
+#include <sstream>
+#include <cstdlib>
 
 #ifndef PHONEBOOK_CLASS_CPP
 # define PHONEBOOK_CLASS_CPP
+
+class Contact
+{
+	private:
+	std::string firstName;
+	std::string lastName;
+	std::string nickname;
+	int phoneNumber;
+	std::string secret;
+
+	public:
+		int add_first(std::string s);
+		int add_last(std::string s);
+		int add_nick(std::string s);
+		int add_number(std::string s);
+		int add_secret(std::string s);
+		void add_contact();
+};
+class PhoneBook
+{
+	private:
+	Contact contacts[8];
+	int	curr_id;
+	public:
+	PhoneBook() : curr_id(0) {} 
+    void add_C();   
+};
 
 #endif
--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -5,15 +5,18 @@
 int main()
 {
 	std:: string cmd;
+	PhoneBook myPhoneBook;
 	std::cout << "Welcome to Phone Book !";
 	std::cout << "Commands: ADD, SEARCH, EXIT"; // add this to loop
 	std::cout << std::endl;
 	while(true)
 	{
-		std:: cin >> cmd;
+		std::cout << "Commands: ADD, SEARCH, EXIT";
+		std::cout << std::endl;
+		std::getline(std::cin, cmd);
 		if (cmd == "ADD")
 		{
-
+			myPhoneBook.add_C();
 		}
 		else if (cmd == "SEARCH")
 		{
@@ -22,12 +25,12 @@ int main()
 		else if (cmd == "EXIT")
 		{
 			std::cout << "Exiting the Phone Book";
+			std::cout << std::endl;
 			return(0);
 		}
 		else
 		{
 			std::cout << "Unknown command. Please try again." << std::endl;
-			std::cout << "Commands: ADD, SEARCH, EXIT";
 		}
 	}
 	return(0);

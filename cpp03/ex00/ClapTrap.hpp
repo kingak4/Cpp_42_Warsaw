0c5e5fde86b0b8c1a3f6e97a8e24f049f406c694
kinga <kinga.kwasniak5@gmail.com>
2025-09-30 14:26:06 +0200

    starting cpp03

---
+++ cpp03/ex00/ClapTrap.hpp
@@ -0,0 +1,25 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+class ClapTrap
+{
+	private:
+		std::string	name;
+		int			hitPoints;
+		int			energyPoints;
+		int			damage;
+	public:
+		ClapTrap(std::string name);
+		ClapTrap(ClapTrap &other);
+		ClapTrap &operator=(ClapTrap &other);
+		~ClapTrap();
+		std::string	getName(void);
+		void 		attack(const std::string& target);
+		void 		takeDamage(unsigned int amount);
+		void 		beRepaired(unsigned int amount);
+};
+
+#endif

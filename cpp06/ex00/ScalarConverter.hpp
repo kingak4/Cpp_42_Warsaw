20c63e67d8183e5a9022c8346828ea90d243bd71
kinga <kinga.kwasniak5@gmail.com>
2025-11-24 18:55:37 +0100

    notes + plan for ex00

--- cpp06/ex00/ScalarConverter.hpp
+++ cpp06/ex00/ScalarConverter.hpp
@@ -1,4 +1,15 @@
 #ifndef SCALARCONVERTER_HPP
 # define SCALARCONVERTER_HPP
 
+class ScalarConverter {
+	public:
+			static void convert(std::string const & literal);
+	private:
+			ScalarConverter();
+			ScalarConverter(ScalarConverter const&);
+			ScalarConverter& operator=(ScalarConverter const&);
+			~ScalarConverter();
+};
+
+
 #endif

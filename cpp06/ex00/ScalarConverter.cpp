888261e0f5df90e0e2f34e7661a40bbda0eaf023
kinga <kinga.kwasniak5@gmail.com>
2025-11-26 15:34:32 +0100

    checker ready

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -29,3 +29,38 @@ std::string trim(char *s)
 	}
 	return(st);
 }
+
+int	checker(std::string s)
+{
+	if (s == "nan" || s == "nanf" || s == "+inf" || s == "-inf"
+		|| s == "+inff" || s == "-inff")
+		return(1);
+	else
+		return(0);
+}
+
+
+// Schemat parsera scalar convertera
+// Trim
+// Usuń spacje z początku i końca stringa.
+// Jeśli po trimie string jest pusty → konwersja niemożliwa.
+// Checker pseudo-literals
+// Sprawdź, czy string to nan, nanf, +inf, -inf, +inff, -inff.
+// Jeśli tak → możesz od razu zwrócić wynik jako float/double.
+// Sprawdzenie znaków w stringu
+// Jeśli string ma spacje w środku → konwersja niemożliwa.
+// Sprawdź, czy są tylko dozwolone znaki:
+// cyfry 0-9
+// kropka . (dla float/double)
+// literka f na końcu (dla float)
+// opcjonalny znak + lub - na początku
+// Rozpoznanie typu literal
+// char → długość = 1 i znak nie jest cyfrą
+// int  same cyfry, ewentualnie +/- na początku
+// float → cyfry + kropka + końcówka f
+// double → cyfry + kropka, bez f
+// Konwersja i castowanie
+// Mając rozpoznany typ podstawowy, możesz przekonwertować na wszystkie inne typy (char, int, float, double) i przygotować do wyświetlenia.
+// Wyświetlanie
+// Zwracasz lub wyświetlasz wynik w odpowiednich formatach.
+// Jeśli konwersja niemożliwa → komunikat typu „impossible” lub „non displayable” dla char.

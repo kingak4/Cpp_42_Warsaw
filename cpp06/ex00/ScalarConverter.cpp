fd66c1c348f244173a3527fd5f5b7ab20d59cdd0
kinga <kinga.kwasniak5@gmail.com>
2025-11-27 20:43:40 +0100

    parser and checker for int

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -37,17 +37,60 @@ int is_char(char *s)
 		{
 			i += 2;
 			if (s[i] == '\'')
-			{
 				return(1);
-			}
 			else
 				return (0);
-			
 		}
 		else
-		{
 			return(0);
-		}
 	}
 	return(0);
+}
+
+int is_nb(char *s)
+{
+	int i = 0;
+	int len = 0;
+
+	while(s[len])
+		len++;
+	if (s[i] == '0' && len == 1)
+		return(1);
+	if(len == 3)
+	{
+		if (s[i] == '\'')
+		{
+			i += 2;
+			if (s[i] == '\'')
+				return(1);
+			else
+				return (0);
+		}
+		else
+			return(0);
+	}
+	if (len >= 4)
+	{
+		if(s[i] == '0')
+			return(0);
+	}
+	if(s[i] == '-' || s[i]  == "+")
+		i++;
+	while (s[i])
+	{
+		if(!isdigit(s[i]))
+			return(0);
+		i++;
+	}
+	char *endptr;
+	errno = 0;
+	long val = strtol(s, &endptr, 10);
+	if (*endptr != '\0') 
+		return(0);
+	if (errno == ERANGE)
+		return(0);
+	if (val < INT_MIN || val > INT_MAX)
+		return(0);
+	int v = val;
+	return(v);
 }

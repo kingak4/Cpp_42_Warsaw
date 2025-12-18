701699722daa088a325540788a3edde14c61763d
kinga <kinga.kwasniak5@gmail.com>
2025-12-18 16:25:02 +0100

    ex01 done

--- cpp06/ex01/Serializer.hpp
+++ cpp06/ex01/Serializer.hpp
@@ -1,4 +1,26 @@
 #ifndef SERIALIZER_HPP
 # define SERIALIZER_HPP
 
+# include <iostream>
+# include <string>
+# include <stdint.h>
+
+struct Data
+{
+	std::string	name;
+	int			age;
+};
+
+class Serializer
+{
+	private:
+		Serializer();
+		Serializer(const Serializer &other);
+		Serializer &operator=(const Serializer &other);
+		~Serializer();
+	public:
+		static uintptr_t serialize(Data* ptr);
+		static Data* deserialize(uintptr_t raw);
+};
+
 #endif
--- cpp06/ex01/main.cpp
+++ cpp06/ex01/main.cpp
@@ -1,2 +1,20 @@
 #include <string>
 #include <iostream>
+#include "Serializer.hpp"
+
+int main()
+{
+	Data data;
+	uintptr_t ptr;
+	Data *otherdata;
+
+	data.age = 20;
+	data.name = "Kinga";
+	std::cout << "Address of data: " << &data << std::endl;
+	std::cout << "Data before serialize: " << data.name << ", " << data.age << std::endl;
+	ptr = Serializer::serialize(&data);
+	std::cout << "Serialized value (uintptr_t): " << ptr << std::endl;
+	otherdata = Serializer::deserialize(ptr);
+	std::cout << "Address after deserialize: " << otherdata << std::endl;
+	std::cout << "Data after deserialize: " << otherdata->name << ", " << otherdata->age << std::endl;
+}
